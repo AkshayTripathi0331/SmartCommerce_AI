c87497800bbfa6f345c701edbc233c6f8cc5d98b
AkshayTripathi0331 <akshaytripathi630@gmail.com>
2024-09-15 21:45:56 +0530

    Added links of login, register , products , and add new products page on home page

--- smartcommerce-ai-frontend/src/app/page.js
+++ smartcommerce-ai-frontend/src/app/page.js
@@ -1,17 +1,23 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-900 mb-6">Welcome to SmartCommerce AI</h1>
+      <h1 className="text-4xl font-bold text-gray-900 mb-6">
+        Welcome to SmartCommerce AI
+      </h1>
       <Link href="/login">
+        <p className="text-blue-500 mb-4">Login</p>
       </Link>
       <Link href="/register">
+        <p className="text-blue-500 mb-4">Register</p>
       </Link>
       <Link href="/products">
-             </Link>
+        <p className="text-blue-500 mb-4">Products</p>
+      </Link>
       <Link href="/products/new">
-          </Link>
+        <p className="text-blue-500 mb-4">Create New Product</p>
+      </Link>
     </div>
   );
 }
